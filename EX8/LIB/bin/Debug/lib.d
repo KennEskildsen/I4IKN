2bb9c7e33da567bdb85d57b617636aff105cafa4
Kenn Eskildsen <kenneskildsen@gmail.com>
2015-09-21 11:38:56 +0200

    oprydning KE

--- EX8/LIB/bin/Debug/lib.d
+++
@@ -1,3 +0,0 @@
-/root/Projects/Exercise_8_c/LIB/bin/Debug/lib.o: \
- /root/Projects/Exercise_8_c/LIB/lib.c \
- /root/Projects/Exercise_8_c/LIB/lib.h
--- EX8/LIB/bin/Release/lib.d
+++
@@ -1,3 +0,0 @@
-/root/Projects/Exercise_8_c/LIB/bin/Release/lib.o: \
- /root/Projects/Exercise_8_c/LIB/lib.c \
- /root/Projects/Exercise_8_c/LIB/lib.h
