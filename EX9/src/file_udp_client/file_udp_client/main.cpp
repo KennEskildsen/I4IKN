2f9c1ef98fe37a84d97dc480570ae44ffe6b9224
KarstenSN <karstenschoun@gmail.com>
2015-10-19 14:25:21 +0200

    uploadede EX9

---
+++ EX9/src/file_udp_client/file_udp_client/main.cpp
@@ -0,0 +1,84 @@
+﻿//============================================================================
+// Name        : file_client.cpp
+// Author      : Lars Mortensen
+// Version     : 1.0
+// Description : file_client in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include "lib.h"
+
+#define BUFLEN 1000
+#define NPACK 10
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  	struct sockaddr_in si_other;
+  	int s, i, portno,recvlen, slen=sizeof(si_other);
+  	char buf[BUFLEN];
+
+    char my_message[8];
+
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(1);
+    }
+
+	// konverter ascii til integer
+    portno = atoi(argv[2]);
+    socklen_t addrlen = sizeof(argv[1]); 
+
+     if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
+       		fprintf(stderr,"ERROR in socket\n");
+
+      memset((char *) &si_other, 0, sizeof(si_other));
+      si_other.sin_family = AF_INET;
+      si_other.sin_port = htons(PORT);
+      if (inet_aton(argv[1], &si_other.sin_addr)==0) {
+         fprintf(stderr, "inet_aton() failed\n");
+         exit(1);
+      }
+         
+    printf("Client connected. Please enter U/u for uptime or L/l for loadavg: ");
+    bzero(my_message,256);
+    scanf("%s",my_message);
+
+    if(my_message[0] == 'U' || my_message[0] == 'u' )
+    		my_message[0] = 'u';
+    else if(my_message[0] == 'l' || my_message[0] == 'L')
+    		my_message[0] = 'l';
+    else
+		{
+		fprintf(stderr,"Error in input\n");
+		exit(1);
+		}
+
+    printf("Sending message %d\n", i);
+       if (sendto(s, my_message, strlen(my_message)+1, 0, (struct sockaddr *)&si_other, slen)==-1)
+       {
+           fprintf(stderr, "ERROR in sendto\n");
+           exit(1);
+       }
+
+	printf("waiting for data %d\n", PORT);
+    recvlen = recvfrom(s, buf, BUFSIZE, 0, (struct sockaddr *)&si_other,&addrlen );
+    printf("received %d bytes\n", recvlen);
+    printf("received: %s\n", buf);
+
+      close(s);
+      return 0;
+    }
+
---
+++ EX9/src/file_udp_server/file_udp_server/main.cpp
@@ -0,0 +1,126 @@
+﻿//============================================================================
+// Name        : file_udp_server.cpp
+// Author      : Mr. Awesome from Royal-Randers 
+// Version     : 1.0
+// Description : file_server in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "lib.h"
+
+#define BUF_SIZE 1000
+
+using namespace std;
+
+void sendFile(const char* fileName, int connfd);
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, connfd, portno;
+     socklen_t clilen;
+     char buffer[256];
+     struct sockaddr_in serv_addr, cli_addr;
+     socklen_t addrlen = sizeof(cli_addr);            /* length of addresses */
+     int n;
+     int recvlen;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+//Her oprettes vores socket. SOCK_DGRAM er UDP connection. AF_INET betyder det er IPV4 protokollen vi benytter
+     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+//bzero sætter alle værdier i en buffer til 0
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+//atoi konverterer ascii to integer. 
+     portno = atoi(argv[1]);
+//sin_family er hvilken type forbindelse der er, her IPV4
+//sin_addr.s_addr sættes den ipadressen på serveren INADDR_ANY henter den selv 
+//sin_port sættes til vores portno, men den skal konverteres til network-byte-order først htons()
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+//bind binder adressen til serveren. Ved fejl er adressen sikkert allerede brugt 
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0){ 
+              close(sockfd);
+              error("ERROR on binding");
+     }
+
+     bzero(buffer,256);
+
+     for (;;) {
+         printf("waiting on port %d\n", PORT);
+         recvlen = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&cli_addr, &addrlen);
+         printf("received %d bytes\n", recvlen);
+         if (recvlen > 0) 
+         {
+                 buffer[recvlen] = 0;
+                 printf("received message: \"%s\"\n", buffer);
+         }
+
+         if(buffer[0]=='u')
+         {
+
+         	      /* Open the file that we wish to transfer */
+                 FILE *fp = fopen("/proc/uptime","r");
+                 if(fp==NULL)
+                 {
+                     printf("\nError opening file\n");
+                     exit(1);
+                 }
+                 unsigned char buff[BUF_SIZE]={0};
+                 int nread = fread(buff,1,BUF_SIZE,fp);
+                 printf("Bytes read from file %d \n", nread);
+
+         	   if (sendto(sockfd, buff, nread, 0, (struct sockaddr *)&cli_addr, addrlen)==-1)
+         	   {
+          			 fprintf(stderr, "ERROR in sendto\n");
+           			 exit(1);
+      		   }
+		 }
+      		   
+           if(buffer[0]=='l')
+         {
+
+         	      /* Open the file that we wish to transfer */
+                 FILE *fp = fopen("/proc/loadavg","r");
+                 if(fp==NULL)
+                 {
+                     printf("\nError opening file\n");
+                     exit(1);
+                 }
+                 unsigned char buff[BUF_SIZE]={0};
+                 int nread = fread(buff,1,BUF_SIZE,fp);
+                 printf("Bytes read from file %d \n", nread);
+
+         	   if (sendto(sockfd, buff, nread, 0, (struct sockaddr *)&cli_addr, addrlen)==-1)
+         	   {
+          			 fprintf(stderr, "ERROR in sendto\n");
+           			 exit(1);
+      		   }
+		 }
+	 }
+         
+
+ 	close(sockfd);
+}	
+
+
+
+
