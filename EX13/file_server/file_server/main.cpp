b09a91a4e6612610560c5eb8e006578cb4799dac
KarstenSN <karstenschoun@gmail.com>
2015-11-11 13:01:21 +0100

    uploadede file_server

--- EX13/file_server/file_server/main.cpp
+++ EX13/file_server/file_server/main.cpp
@@ -10,13 +10,13 @@ Link *linklayer;
 int main(int argc, char** argv)
 {
 	linklayer = new Link(BUFSIZE);
-	char buf[]="HELLO WORLD!";
-	linklayer->send(buf,12);
+	//char buf[]="HELLO WORLD!";
+	//linklayer->send(buf,12);
 
-	//char buf2[BUFSIZE]={0};
-	//linklayer->receive(buf2,BUFSIZE);
+	char buf2[BUFSIZE]={0};
+	linklayer->receive(buf2,BUFSIZE);
 
-	//cout<<"Got the message: "<<buf2<<endl;
+	cout<<"Got the message: "<<buf2<<endl;
 	
 	return 0;
 }
